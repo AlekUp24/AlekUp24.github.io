dc7150c84cf228ce6effb04fe593255d558645e2
AlekUp24 <alekp24@gmail.com>
2024-07-16 08:55:36 +0200

    CV uploaded

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
   images.forEach(function (image) {
     image.addEventListener("click", function () {
       this.classList.toggle("zoomed");
-      console.log("clicked");
     });
   });
 });
